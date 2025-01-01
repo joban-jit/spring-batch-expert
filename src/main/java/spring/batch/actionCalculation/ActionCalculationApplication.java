d58dbb704a1d94052b49abfbd251e7cf583e5c97
Jobanjit Singh <joban@192.168.1.11>
2025-01-01 11:36:47 +0530

    initial commit

---
+++ src/main/java/spring/batch/actionCalculation/ActionCalculationApplication.java
@@ -0,0 +1,13 @@
+package spring.batch.actionCalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionCalculationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/batch/actionCalculation/ActionCalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.batch.actionCalculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionCalculationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
